3fe81ffe9fb1511ed5b828608c7577968c969fd1
MaxGornakoff <max.gornakoff@yandex.ru>
2019-12-27 15:47:58 +0300

    test for job

---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">	
+	
+	<title>Тестовое задание</title>
+	
+	<link href="css/reset.css" type="text/css" rel="stylesheet"  media="screen"/>
+	<link href="css/overheight.css" type="text/css" rel="stylesheet"  media="screen"/>
+	<link href="css/style.css" type="text/css" rel="stylesheet"  media="screen"/>
+		
+	<script src="jquery-3.4.1.min.js"></script>
+	<script src="scripts.js"></script>
+	<script src="overheight/overheight.js"></script>
+		
+</head>
+
+<body>
+	<header class="header">
+		<div class="container">
+			<div class="header__mobile-menu-wrap">
+				<a href="#" title="Menu" class="header__mobile-menu" id="menu_button" content="main_menu" onclick="content_overheight(this); return;"><span></span></a>
+			</div>
+			<div class="header__area--left">
+				<div class="header__logo-wrap">
+					<a class="header__logo" href="/"></a>
+				</div>
+				<div class="header__contacts-wrap">
+					<div class="header__city-wrap">
+						<p class="header__city-title">Ваш город:</p>
+						<span class="header__city-select">Краснодар</span>
+					</div>
+					<div class="header__phone-wrap">
+						<a class="header__phone-number" href="tel:+79186566378">8 (918) 656-63-78</a>
+						<a class="header__phone-recall" href="#">Заказать обратный звонок</a>
+					</div>
+				</div>
+			</div>
+			<div class="header__area--middle">
+				<div class="header__social-wrap">
+					<a class="header__whatsapp" href="#"></a>
+					<a class="header__telegram" href="#"></a>
+					<a class="header__viber" href="#"></a>
+				</div>
+				<div class="header__search-wrap">
+					<label for="search"></label>
+					<input type="search" name="search" id="search" placeholder="Поиск по каталогу">
+				</div>
+				<div class="header__menu-wrap" id="top_menu">
+					<nav>
+						<ul>
+							<li>
+								<a href="#">О компании</a>
+							</li>
+							<li>
+								<a href="#">Доставка и оплата</a>
+							</li>
+							<li>
+								<a href="#">Оснащение ресторанов и офисов</a>
+							</li>
+							<li>
+								<a href="#">Дизайнерам</a>
+							</li>
+							<li>
+								<a href="#">Контакты</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<div class="header__area--right">
+				<div class="header__login-wrap">
+					<a class="header__login">Войти</a>
+				</div>
+				<div class="header__basket-wrap">
+					<a class="header__wishlist" href="#"><span></span></a>
+					<a class="header__basket" href="#"><span></span></a>
+				</div>
+			</div>
+			<div class="over-container" id="over-container">
+				<div class="hidden-content__header">
+					<div class="header__contacts-wrap">
+						<div class="header__city-wrap">
+							<p class="header__city-title">Ваш город:</p>
+							<span class="header__city-select">Краснодар</span>
+						</div>
+						<div class="header__phone-wrap">
+							<a class="header__phone-number" href="tel:+79186566378">8 (918) 656-63-78</a>
+							<a class="header__phone-recall" href="#">Заказать обратный звонок</a>
+						</div>
+					</div>
+					<div class="header__login-wrap">
+						<a class="header__login">Войти</a>
+					</div>
+				</div>
+				
+				<div class="hidden-content__menu">
+						<div class="header__menu-wrap" id="top_menu">
+							<nav>
+								<ul>
+									<li>
+										<a href="#">О компании</a>
+									</li>
+									<li>
+										<a href="#">Доставка и оплата</a>
+									</li>
+									<li>
+										<a href="#">Оснащение ресторанов и офисов</a>
+									</li>
+									<li>
+										<a href="#">Дизайнерам</a>
+									</li>
+									<li>
+										<a href="#">Контакты</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+    			<div class="over-content" id="over-content">
+				</div>
+				<div class="header__social-wrap">
+					<a class="header__whatsapp" href="#"></a>
+					<a class="header__telegram" href="#"></a>
+					<a class="header__viber" href="#"></a>
+				</div>
+			</div>	
+		</div>
+	</header>
+	<nav class="main-menu" id="main_menu">
+		<div class="container menu-container">
+			<ul>
+				<li class="list-item-1">
+					<a href="#"><span></span>Распродажа</a>
+				</li>
+				<li class="list-item-2">
+					<a href="#"><span></span>Бренды</a>
+				</li>
+				<li class="list-item-3">
+					<a href="#"><span></span>Люстры</a>
+				</li>
+				<li class="list-item-4">
+					<a href="#"><span></span>Светильники</a>
+				</li>
+				<li class="list-item-5">
+					<a href="#"><span></span>Бра</a>
+				</li>
+				<li class="list-item-6">
+					<a href="#"><span></span>Настольные</a>
+				</li>
+				<li class="list-item-7">
+					<a href="#"><span></span>Торшеры</a>
+				</li>
+				<li class="list-item-8">
+					<a href="#"><span></span>Лампы</a>
+				</li>
+				<li class="list-item-9">
+					<a href="#"><span></span>Интересное</a>
+				</li>
+				<li class="list-item-10">
+					<a href="#"><span></span>Часы</a>
+				</li>
+				<li class="list-item-11">
+					<a href="#"><span></span>+ еще 7</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	
+	
+<script>
+	</script>
+</body>
+</html>
